139c9abb8a20bf8b2cbe49ff788191f1a48aa9e4
SRaylyan <raylan@mail.ru>
2019-05-08 12:37:25 +0400

    Add files via upload

---
+++ precache-manifest.514bd4dc4fcfaa23ec9049c16813dbb3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d7c31b284ddb01fc1cbae0edc9ec210",
+    "url": "./static/media/Calibri-Regular.5d7c31b2.ttf"
+  },
+  {
+    "revision": "bcff7ce60d6027283f381aea86ae83f8",
+    "url": "./static/media/Calibri-Bold.bcff7ce6.woff"
+  },
+  {
+    "revision": "7b7e5f5b806d15f691443b465dd4ff44",
+    "url": "./static/media/Calibri-Bold.7b7e5f5b.eot"
+  },
+  {
+    "revision": "527c5ab608cab860a6aae8ce02e14b0e",
+    "url": "./static/media/Calibri-Bold.527c5ab6.ttf"
+  },
+  {
+    "revision": "4c20fe4ef6e5a4cfa62eef2b6c90a861",
+    "url": "./static/media/Calibri-Bold.4c20fe4e.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e8fe89750b31424ce195",
+    "url": "./static/js/main.7b891668.chunk.js"
+  },
+  {
+    "revision": "5cadba1eab1ef5fc4498",
+    "url": "./static/js/2.62485115.chunk.js"
+  },
+  {
+    "revision": "e8fe89750b31424ce195",
+    "url": "./static/css/main.06617481.chunk.css"
+  },
+  {
+    "revision": "d338cb526c0f48ab91d4c7b04cc004e7",
+    "url": "./index.html"
+  }
+];
